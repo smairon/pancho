7e2c3c2b7f92708c37550b4452403078d61285b5
smairon <man@smairon.ru>
2024-11-02 20:19:31 +0300

    - Added raising exception when error occurs for informing di context manager

--- pancho/implementation/execution.py
+++ pancho/implementation/execution.py
@@ -3,6 +3,10 @@ from ..definition import contracts
 from .processing import CQProcessor
 
 
+class ExpectedErrorOccurred(Exception):
+    pass
+
+
 class TaskExecutor:
     def __init__(
         self,
@@ -19,8 +23,13 @@ class TaskExecutor:
     ) -> list[contracts.Message]:
         resolver_context = (execution_context,) if execution_context else ()
         stream = []
-        async with self._di_container.get_resolver(*resolver_context) as resolver:
-            processor = CQProcessor(self._actor_registry, resolver)(task)
-            async for message in processor:
-                stream.append(message)
+        try:
+            async with self._di_container.get_resolver(*resolver_context) as resolver:
+                processor = CQProcessor(self._actor_registry, resolver)(task)
+                async for message in processor:
+                    stream.append(message)
+                    if isinstance(message, contracts.Error):
+                        raise ExpectedErrorOccurred  # raise error just for informing context manager
+        except ExpectedErrorOccurred:
+            pass  # supress this artificial error
         return stream
